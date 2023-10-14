dbe975e5ec1c7e222691c8819b125f4f87b60def
Bolu <boluelbot@gmail.com>
2023-10-14 23:12:32 +0200

    Fix message order and add negative bonus

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@
 use std::env;
 use regex::Regex;
 extern crate reqwest;
-
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 use serenity::Client;
@@ -33,56 +32,29 @@ impl EventHandler for Bot {
             return;
         }
 
+        let mut content = msg.content;
+
         // Regular roll message, e.g.: [2d20]
-        let dice = Regex::new(r"\[(\d+)d(\d+)\]").expect("No regex?");
-        for (_, [rolls_str, size_str]) in dice.captures_iter(&msg.content).map(|c| c.extract()) {
-            let rolls = rolls_str.parse::<u32>().expect("No rolls?");
-            let size = size_str.parse::<u32>().expect("No size?");
-            
-            // Arbitrary limits check, so only reasonable amounts of numbers of reasonable size are returned;
-            if rolls > 20u32 {
-                let _ = msg.channel_id.send_message(&ctx, |msg| msg.content("Inquired for overmuch rolls. I may only proffer up to twain score!")).await;
-                continue;
-            }
-            if size > 1_000u32 {
-                let _ = msg.channel_id.send_message(&ctx, |msg| msg.content("Entreaded for an excessive sum. I can only reckon unto a thousand!")).await;
-                continue;
-            }
+        let dice = Regex::new(r"(?<roll>\[\d+d\d+)\]").expect("No un-bonused regex?");
+        content = dice.replace_all(&content, "$roll+0]").into_owned();
 
-            let url = format!("https://www.random.org/integers/?num={}&min=1&max={}&col=1&base=10&format=plain&rnd=new", rolls, size);
-            let res = reqwest::get(url).await.expect("No random?");
-            let body = res.text().await.expect("No numbers?");
-
-            let mut sequence = format!("{}", body.replace("\n", ", "));
-            sequence.pop(); // Remove trailing space
-            sequence.pop(); // Remove trailing comma
-
-            if rolls > 1 {
-                let sum = sequence.split(", ").map(|n| n.parse::<u32>().unwrap()).reduce(|a, b| a + b).expect("No reduction?");
-
-                let _ = msg.channel_id.send_message(&ctx, |msg| msg.content(format!("{} = {}", sequence, sum))).await;
-            } else {
-                let _ = msg.channel_id.send_message(&ctx, |msg| msg.content(format!("{}", sequence))).await;
-            }
-        }
-
-        // FIXME: As it is right now, first all non-bonus roll will be processed, then all bonus rolls.
-        // Fix so that all rolls are processed in order, regardless of bonus.
-        // (I.e.: use a regex that captures an optional group for the bonus.)
+        // Negative bonus roll message, e.g.: [2d20-5]
+        let dice_and_neg_bonus = Regex::new(r"(?<roll>\[\d+d\d+) ?- ?(?<bonus>\d+\])").expect("No negative-bonused regex?");
+        content = dice_and_neg_bonus.replace_all(&content, "$roll+-$bonus").into_owned();
 
         // Bonus roll message, e.g.: [2d20+5]
-        let dice_and_bonus = Regex::new(r"\[(\d+)d(\d+) ?\+ ?(\d+)\]").expect("No regex?");
-        for (_, [rolls_str, size_str, bonus_str]) in dice_and_bonus.captures_iter(&msg.content).map(|c| c.extract()) {
-            let rolls = rolls_str.parse::<u32>().expect("No rolls?");
-            let size = size_str.parse::<u32>().expect("No size?");
-            let bonus = bonus_str.parse::<u32>().expect("No bonus?");
+        let dice_and_bonus = Regex::new(r"\[(\d+)d(\d+) ?\+ ?(-?\d+)\]").expect("No regex?");
+        for (_, [rolls_str, size_str, bonus_str]) in dice_and_bonus.captures_iter(&content).map(|c| c.extract()) {
+            let rolls = rolls_str.parse::<i32>().expect("No rolls?");
+            let size = size_str.parse::<i32>().expect("No size?");
+            let bonus = bonus_str.parse::<i32>().expect("No bonus?");
             
             // Arbitrary limits check, so only reasonable amounts of numbers of reasonable size are returned;
-            if rolls > 20u32 {
+            if rolls > 20i32 {
                 let _ = msg.channel_id.send_message(&ctx, |msg| msg.content("Inquired for overmuch rolls. I may only proffer up to twain score!")).await;
                 continue;
             }
-            if size > 1_000u32 {
+            if size > 1_000i32 {
                 let _ = msg.channel_id.send_message(&ctx, |msg| msg.content("Entreaded for an excessive sum. I can only reckon unto a thousand!")).await;
                 continue;
             }
@@ -91,17 +63,34 @@ impl EventHandler for Bot {
                 continue;
             }
 
-            let url = format!("https://www.random.org/integers/?num={}&min=1&max={}&col=1&base=10&format=plain&rnd=new", rolls, size);
-            let res = reqwest::get(url).await.expect("No random?");
-            let body = res.text().await.expect("No numbers?");
+            // Roll sequence generation:
+            let mut sequence: String;
 
-            let mut sequence = format!("{}", body.replace("\n", ", "));
+            if size > 1 {
+                let url = format!("https://www.random.org/integers/?num={}&min=1&max={}&col=1&base=10&format=plain&rnd=new", rolls, size);
+                let res = reqwest::get(url).await.expect("No random?");
+                let body = res.text().await.expect("No numbers?");
+
+                sequence = format!("{}", body.replace("\n", ", "));
+            } else {
+                sequence = size.to_string();
+                sequence.push_str(", ");
+                sequence = sequence.repeat(rolls as usize);
+            }
             sequence.pop(); // Remove trailing space
             sequence.pop(); // Remove trailing comma
 
-            let sum = sequence.split(", ").map(|n| n.parse::<u32>().unwrap()).reduce(|a, b| a + b).expect("No reduction?");
+            let sum = sequence.split(", ").map(|n| n.parse::<i32>().unwrap()).reduce(|a, b| a + b).expect("No reduction?");
 
-            let _ = msg.channel_id.send_message(&ctx, |msg| msg.content(format!("{} + {} = {}", sequence, bonus, sum + bonus))).await;
+            if bonus == 0 {
+                if rolls == 1 {
+                    let _ = msg.channel_id.send_message(&ctx, |msg| msg.content(format!("{}", sequence))).await;
+                } else {
+                    let _ = msg.channel_id.send_message(&ctx, |msg| msg.content(format!("{} = {}", sequence, sum + bonus))).await;
+                }
+            } else {
+                let _ = msg.channel_id.send_message(&ctx, |msg| msg.content(format!("{} + {} = {}", sequence, bonus, sum + bonus))).await;
+            }
         }
     }
 
